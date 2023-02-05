94b0e366bc003dadddf16954d4f92632cc7ca6de
Matheo <matheo.martin@epitech.eu>
2023-02-05 22:30:51 +0100

    [Upload]

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo pacman -S alacritty git htop neovim zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions base-devel
+mkdir ~/.Documents/Personal_project
+cp -r .config ~/.config
+cp -r .zshrc ~/.zshrc
