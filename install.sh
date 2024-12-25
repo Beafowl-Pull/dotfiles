3f1f67caa2f3df17eef9e72663bb1c37e806821f
Beafowl <85102501+Beafowl-Pull@users.noreply.github.com>
2024-12-25 20:19:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,54 +1,54 @@
 #!/usr/bin/env bash
 
-install-yay() {
+install_yay() {
   sudo pacman -S --needed --noconfirm base-devel
   git clone https://aur.archlinux.org/yay-bin.git /tmp/buildyay
   cd /tmp/buildyay
   makepkg -o
   makepkg -se
   makepkg -i --noconfirm
-  cd $base
+  cd ~
   rm -rf /tmp/buildyay
 }
 
-install-requirements() {
-  sudo pacman -Syyuu
+install_requirements() {
+  sudo pacman -Syyuu --noconfirm
   sudo pacman -S pipewire libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon gvfs
-  sudo pacman -S git kitty firefox zsh alacritty thunar tumbler discord htop cmake python-pywal sxiv unzip wpgtk nwg-look
-  yay -S grimblast-git gpu-screen-recorder hyprpicker matugen-bin python-gpustat aylurs-gtk-shell-git hyprsunset-git hypridle-git lens-bin jetbrains-toolbox spotify visual-studio-code-bin python-pywalfox pywal-discord-git spicetify-cli
+  sudo pacman -S git kitty firefox zsh alacritty thunar tumbler discord htop cmake python-pywal sxiv unzip wpgtk nwg-look --noconfirm
+  yay -S grimblast-git gpu-screen-recorder hyprpicker matugen-bin python-gpustat aylurs-gtk-shell-git hyprsunset-git hypridle-git lens-bin jetbrains-toolbox spotify visual-studio-code-bin python-pywalfox pywal-discord-git spicetify-cli --noconfirm
 }
 
-install-better-discord() {
+install_better_discord() {
   curl -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
   chmod +x betterdiscordctl
   sudo mv betterdiscordctl /usr/local/bin
 }
 
-install-zsh-requirements() {
+install_zsh_requirements() {
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/loket/oh-my-zsh/feature/batch-mode/tools/install.sh)" -s --batch
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 }
 
-install-rofi-requirements() {
+install_rofi_requirements() {
   git clone --depth=1 https://github.com/adi1090x/rofi.git
   cd rofi
   chmod +x setup.sh
   ./setup.sh
 }
 
-copy-dotfiles() {
+copy_dotfiles() {
   cp -r Pictures .config .p10k.zsh .zshrc ~/
 }
 
-install-spotify-theme() {
+install_spotify_theme() {
   git clone https://github.com/Comfy-Themes/Spicetify
   cd Spicetify
   ./install.sh
 }
 
-init-some-stuff() {
+init_some_stuff() {
   swww img ~/Pictures/wallpaper.png
   wal -n -i ~/Pictures/$img
   sh ~/.config/hypr/scripts/changecolorsrofi.sh
@@ -60,28 +60,31 @@ init-some-stuff() {
   pywal-discord
 }
 
-install-ags-theme() {
-  git clone https://github.com/Jas-SinghFSU/HyprPanel.git ~/Documents && ln -s $(pwd)/HyprPanel $HOME/.config/ags
+install_ags_theme() {
+  git clone https://github.com/Jas-SinghFSU/HyprPanel.git ~/Documents && ln -s ~/Documents/HyprPanel $HOME/.config/ags
 }
 
-install-sddm-theme() {
-  sudo pacman -S qt5-graphicaleffects qt5-quickcontrols2 qt5-svg sddm
+install_sddm_theme() {
+  sudo pacman -S qt5-graphicaleffects qt5-quickcontrols2 qt5-svg sddm --noconfirm
   sudo git clone https://github.com/keyitdev/sddm-flower-theme.git /usr/share/sddm/themes/sddm-flower-theme
   sudo cp /usr/share/sddm/themes/sddm-flower-theme/Fonts/* /usr/share/fonts/
   echo "[Theme]
   Current=sddm-flower-theme" | sudo tee /etc/sddm.conf
 }
 
-install-yay()
-install-requirements()
-install-better-discord()
-install-zsh-requirements()
-install-rofi-requirements()
-install-spotify-theme()
-install-ags-theme()
-install-sddm-theme()
-copy-dotfiles()
-init-some-stuff()
+# Call the functions in sequence
+install_yay
+install_requirements
+install_better_discord
+install_zsh_requirements
+install_rofi_requirements
+install_spotify_theme
+install_ags_theme
+install_sddm_theme
+copy_dotfiles
+init_some_stuff
+
+# Enable and start systemd services
 sudo systemctl enable nvidia-hibernate nvidia-suspend nvidia-resume
 sudo systemctl enable sddm
 sudo systemctl start sddm
